0f5a84814ef392712f7125d6ee3a71f5fe1e4804
Tarik2021 <tariks2021@gmail.com>
2021-03-30 10:01:52 +0200

    Day22 exercises

---
+++ ex10/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import Box from "./Box"
+
+function App() {
+  return <Box size="20" />;
+}
+
+export default App;
---
+++ ex10/src/Box.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Box(props) {
+  return <h2>This is a {this.props.size}px Box</h2>;
+}
+
+export default Box;
